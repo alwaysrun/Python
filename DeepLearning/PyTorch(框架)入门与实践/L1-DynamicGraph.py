48345a4a370e9982858d5a43d14ce8b0653b6925
alwaysrun <guodxu59@126.com>
2019-12-01 18:50:31 +0800

    Create L1-DynamicGraph.py

---
+++ DeepLearning/PyTorch(框架)入门与实践/L1-DynamicGraph.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 23 22:50:17 2019
+
+@author: guodx
+"""
+
+import torch as th
+from torch.autograd import Variable
+
+N, D, H  = 3, 4, 5
+
+x=Variable(th.randn(N,D))
+# print(x)
+w1=Variable(th.randn(D, H))
+w2=Variable(th.randn(D,H))
+
+z = th.randint(10, [1])
+print(z)
+
+if(z[0]>5):
+    y=x.mm(w1)
+else:
+    y=x.mm(w2)
+    
+print(y)
